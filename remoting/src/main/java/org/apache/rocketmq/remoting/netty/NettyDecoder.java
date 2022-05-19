0ad23bb88bceb0581f1cd2182537b02dc803495a
Shipin <shpzhao@rxhui.com>
2022-05-19 17:50:15 +0800

    coder

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyDecoder.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyDecoder.java
@@ -26,6 +26,9 @@ import org.apache.rocketmq.logging.InternalLogger;
 import org.apache.rocketmq.logging.InternalLoggerFactory;
 import org.apache.rocketmq.remoting.protocol.RemotingCommand;
 
+/**
+ * byt转RemotingCommand
+ */
 public class NettyDecoder extends LengthFieldBasedFrameDecoder {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(RemotingHelper.ROCKETMQ_REMOTING);
 
